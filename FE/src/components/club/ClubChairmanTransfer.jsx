7fb992ec63fa8fae7615119f153464ae78f214d5
Hoangtuan2005 <Tuan07375@gmail.com>
2026-06-05 10:34:27 +0700

    Remove internal K19 email note from chairman transfer warning text.

--- FE/src/components/club/ClubChairmanTransfer.jsx
+++ FE/src/components/club/ClubChairmanTransfer.jsx
@@ -65,8 +65,7 @@ const ClubChairmanTransfer = ({ showToast, compact = false, onTransferred }) => 
           <div className="clb-chairman-transfer__alert">
             <strong>Lưu ý quan trọng</strong>
             <p>
-              Sau khi chuyển nhượng, bạn sẽ <em>mất quyền quản lý CLB</em>. Người nhận phải có tài khoản email trong trường
-              (Vì sinh viên K19 trở đi dùng mail cá nhân rồi).
+              Sau khi chuyển nhượng, bạn sẽ <em>mất quyền quản lý CLB</em>. Người nhận phải có tài khoản email trong trường.
             </p>
           </div>
           <form className="clb-chairman-transfer__form" onSubmit={handleSubmit}>
