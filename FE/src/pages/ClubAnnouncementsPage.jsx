78aa1ad2964e4dc7d35488f02c3b9debc741adcf
Thuan20Vnx <tranxuanthuan20@gmail.com>
2026-06-06 12:25:48 +0700

    Keep club management sidebar visible on club announcement pages.

--- FE/src/pages/ClubAnnouncementsPage.jsx
+++ FE/src/pages/ClubAnnouncementsPage.jsx
@@ -1,18 +1,13 @@
 import React from 'react';
-import SiteHeader from '../components/SiteHeader';
-import SiteFooter from '../components/SiteFooter';
+import ClubPortalShell from '../layouts/ClubPortalShell';
 import { ClubAnnouncementManage } from './ctsv/CtsvAnnouncementPublish';
 
 const ClubAnnouncementsPage = ({ showToast }) => (
-  <div className="announcements-page">
-    <SiteHeader activeNav="club-manage" />
-    <main className="announcements-page__main">
-      <div className="announcements-page__container" style={{ maxWidth: 'var(--home-content-max, 1280px)' }}>
-        <ClubAnnouncementManage showToast={showToast} />
-      </div>
-    </main>
-    <SiteFooter />
-  </div>
+  <ClubPortalShell activeNav="announcements" showToast={showToast}>
+    <div className="announcements-page__container" style={{ width: '100%', maxWidth: 'var(--home-content-max, 1280px)', margin: '0 auto' }}>
+      <ClubAnnouncementManage showToast={showToast} />
+    </div>
+  </ClubPortalShell>
 );
 
 export default ClubAnnouncementsPage;
