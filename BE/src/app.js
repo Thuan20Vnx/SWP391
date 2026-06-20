af4a7aed4a53201d08617cd1705254b7d6e90700
Thuan20Vnx <tranxuanthuan20@gmail.com>
2026-06-20 13:54:50 +0700

    debug: add /db-info endpoint to verify production DB connection

--- BE/src/app.js
+++ BE/src/app.js
@@ -32,6 +32,16 @@ app.get('/', (req, res) => {
   res.send('SWP391 API Server is running...');
 });
 
+app.get('/db-info', (req, res) => {
+  const mongoose = require('mongoose');
+  const { connection } = mongoose;
+  res.json({
+    state: connection.readyState,
+    host: connection.host || null,
+    name: connection.name || null,
+  });
+});
+
 app.use('/api', dbReady, apiRoutes);
 
 app.use(notFoundHandler);
